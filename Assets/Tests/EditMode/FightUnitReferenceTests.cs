07a05ed44d8b7ebfd66413048e9e85466323d86e
JedrzejMakuch <jedrzejmakuch@gmail.com>
2026-07-13 17:15:11 +0200

    Add game assembly reference for tests

---
+++ Assets/Tests/EditMode/FightUnitReferenceTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace DiceBossArena.Tests.EditMode
+{
+    public sealed class FightUnitReferenceTests
+    {
+        [Test]
+        public void TestAssembly_CanCreateFightUnit()
+        {
+            GameObject gameObject = new GameObject("Test FightUnit");
+
+            try
+            {
+                FightUnit unit = gameObject.AddComponent<FightUnit>();
+
+                Assert.That(unit, Is.Not.Null);
+            }
+            finally
+            {
+                Object.DestroyImmediate(gameObject);
+            }
+        }
+    }
+}
