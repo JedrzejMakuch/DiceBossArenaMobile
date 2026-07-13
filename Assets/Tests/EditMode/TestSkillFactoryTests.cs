96dfed8235560b36f86aa0c5d1d749621c50fa50
JedrzejMakuch <jedrzejmakuch@gmail.com>
2026-07-13 17:32:20 +0200

    Add test skill factory

---
+++ Assets/Tests/EditMode/TestSkillFactoryTests.cs
@@ -0,0 +1,28 @@
+﻿using DiceBossArena.Tests.Fixtures;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace DiceBossArena.Tests.EditMode
+{
+    public sealed class TestSkillFactoryTests
+    {
+        [Test]
+        public void Create_ReturnsSkillDefinition()
+        {
+            SkillDefinition skill = TestSkillFactory.Create();
+
+            try
+            {
+                Assert.That(skill, Is.Not.Null);
+                Assert.That(skill.name, Is.EqualTo("Test Skill"));
+                Assert.That(skill.MaxLevel, Is.EqualTo(1));
+                Assert.That(skill.MaxRange, Is.EqualTo(1));
+                Assert.That(skill.ActionPointCost, Is.EqualTo(1));
+            }
+            finally
+            {
+                Object.DestroyImmediate(skill);
+            }
+        }
+    }
+}
---
+++ Assets/Tests/Fixtures/TestSkillFactory.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace DiceBossArena.Tests.Fixtures
+{
+    public static class TestSkillFactory
+    {
+        public static SkillDefinition Create()
+        {
+            SkillDefinition skill =
+                ScriptableObject.CreateInstance<SkillDefinition>();
+
+            skill.name = "Test Skill";
+
+            return skill;
+        }
+    }
+}
