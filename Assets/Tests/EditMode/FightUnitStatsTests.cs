af837543218b41bccdf1960c523bce6b23718115
JedrzejMakuch <jedrzejmakuch@gmail.com>
2026-07-14 13:47:55 +0200

    Add runtime fight unit stats

---
+++ Assets/Tests/EditMode/FightUnitStatsTests.cs
@@ -0,0 +1,291 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using DiceBossArena.Game;
+
+namespace DiceBossArena.Tests.EditMode
+{
+    public sealed class FightUnitStatsTests
+    {
+        [Test]
+        public void Constructor_CopiesInitialBaseValues()
+        {
+            Dictionary<FightStatType, int> baseValues =
+                new()
+                {
+                    {
+                        FightStatType.MaxHealth,
+                        20
+                    },
+                    {
+                        FightStatType.AttackPower,
+                        5
+                    }
+                };
+
+            FightUnitStats stats =
+                new FightUnitStats(baseValues);
+
+            Assert.That(
+                stats.GetBaseValue(
+                    FightStatType.MaxHealth),
+                Is.EqualTo(20));
+
+            Assert.That(
+                stats.GetBaseValue(
+                    FightStatType.AttackPower),
+                Is.EqualTo(5));
+        }
+
+        [Test]
+        public void GetBaseValue_ForMissingStat_ReturnsZero()
+        {
+            Dictionary<FightStatType, int> baseValues =
+                new();
+
+            FightUnitStats stats =
+                new FightUnitStats(baseValues);
+
+            Assert.That(
+                stats.GetBaseValue(
+                    FightStatType.Initiative),
+                Is.Zero);
+        }
+
+        [Test]
+        public void Constructor_DoesNotKeepReferenceToSourceDictionary()
+        {
+            Dictionary<FightStatType, int> baseValues =
+                new()
+                {
+                    {
+                        FightStatType.AttackPower,
+                        10
+                    }
+                };
+
+            FightUnitStats stats =
+                new FightUnitStats(baseValues);
+
+            baseValues[FightStatType.AttackPower] =
+                999;
+
+            Assert.That(
+                stats.GetBaseValue(
+                    FightStatType.AttackPower),
+                Is.EqualTo(10));
+        }
+
+        [Test]
+        public void GetFinalValue_AppliesModifiers()
+        {
+            Dictionary<FightStatType, int> baseValues =
+                new()
+                {
+            {
+                FightStatType.AttackPower,
+                10
+            }
+                };
+
+            FightUnitStats stats =
+                new FightUnitStats(baseValues);
+
+            stats.AddModifier(
+                new FightStatModifier(
+                    FightStatType.AttackPower,
+                    FightStatModifierType.Flat,
+                    5));
+
+            stats.AddModifier(
+                new FightStatModifier(
+                    FightStatType.AttackPower,
+                    FightStatModifierType.Percent,
+                    20));
+
+            Assert.That(
+                stats.GetFinalValue(
+                    FightStatType.AttackPower),
+                Is.EqualTo(18));
+        }
+
+        [Test]
+        public void AddModifier_AffectsOnlyMatchingStat()
+        {
+            Dictionary<FightStatType, int> baseValues =
+                new()
+                {
+            {
+                FightStatType.AttackPower,
+                10
+            },
+            {
+                FightStatType.MaxHealth,
+                20
+            }
+                };
+
+            FightUnitStats stats =
+                new FightUnitStats(baseValues);
+
+            stats.AddModifier(
+                new FightStatModifier(
+                    FightStatType.AttackPower,
+                    FightStatModifierType.Flat,
+                    5));
+
+            Assert.That(
+                stats.GetFinalValue(
+                    FightStatType.AttackPower),
+                Is.EqualTo(15));
+
+            Assert.That(
+                stats.GetFinalValue(
+                    FightStatType.MaxHealth),
+                Is.EqualTo(20));
+        }
+
+        [Test]
+        public void SeparateInstances_DoNotShareModifiers()
+        {
+            Dictionary<FightStatType, int> baseValues =
+                new()
+                {
+            {
+                FightStatType.AttackPower,
+                10
+            }
+                };
+
+            FightUnitStats firstStats =
+                new FightUnitStats(baseValues);
+
+            FightUnitStats secondStats =
+                new FightUnitStats(baseValues);
+
+            firstStats.AddModifier(
+                new FightStatModifier(
+                    FightStatType.AttackPower,
+                    FightStatModifierType.Flat,
+                    5));
+
+            Assert.That(
+                firstStats.GetFinalValue(
+                    FightStatType.AttackPower),
+                Is.EqualTo(15));
+
+            Assert.That(
+                secondStats.GetFinalValue(
+                    FightStatType.AttackPower),
+                Is.EqualTo(10));
+        }
+
+        [Test]
+        public void RemoveModifier_RemovesMatchingModifier()
+        {
+            Dictionary<FightStatType, int> baseValues =
+                new()
+                {
+            {
+                FightStatType.AttackPower,
+                10
+            }
+                };
+
+            FightUnitStats stats =
+                new FightUnitStats(baseValues);
+
+            FightStatModifier modifier =
+                new FightStatModifier(
+                    FightStatType.AttackPower,
+                    FightStatModifierType.Flat,
+                    5);
+
+            stats.AddModifier(modifier);
+
+            bool removed =
+                stats.RemoveModifier(modifier);
+
+            Assert.That(
+                removed,
+                Is.True);
+
+            Assert.That(
+                stats.GetFinalValue(
+                    FightStatType.AttackPower),
+                Is.EqualTo(10));
+        }
+
+        [Test]
+        public void RemoveModifier_WhenModifierDoesNotExist_ReturnsFalse()
+        {
+            Dictionary<FightStatType, int> baseValues =
+                new()
+                {
+            {
+                FightStatType.AttackPower,
+                10
+            }
+                };
+
+            FightUnitStats stats =
+                new FightUnitStats(baseValues);
+
+            FightStatModifier modifier =
+                new FightStatModifier(
+                    FightStatType.AttackPower,
+                    FightStatModifierType.Flat,
+                    5);
+
+            bool removed =
+                stats.RemoveModifier(modifier);
+
+            Assert.That(
+                removed,
+                Is.False);
+        }
+
+        [Test]
+        public void ClearModifiers_RemovesAllModifiers()
+        {
+            Dictionary<FightStatType, int> baseValues =
+                new()
+                {
+            {
+                FightStatType.AttackPower,
+                10
+            },
+            {
+                FightStatType.MaxHealth,
+                20
+            }
+                };
+
+            FightUnitStats stats =
+                new FightUnitStats(baseValues);
+
+            stats.AddModifier(
+                new FightStatModifier(
+                    FightStatType.AttackPower,
+                    FightStatModifierType.Flat,
+                    5));
+
+            stats.AddModifier(
+                new FightStatModifier(
+                    FightStatType.MaxHealth,
+                    FightStatModifierType.Percent,
+                    50));
+
+            stats.ClearModifiers();
+
+            Assert.That(
+                stats.GetFinalValue(
+                    FightStatType.AttackPower),
+                Is.EqualTo(10));
+
+            Assert.That(
+                stats.GetFinalValue(
+                    FightStatType.MaxHealth),
+                Is.EqualTo(20));
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Fight/Stats/FightUnitStats.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiceBossArena.Game
+{
+    public sealed class FightUnitStats
+    {
+        private readonly Dictionary<FightStatType, int> baseValues =
+            new();
+
+        private readonly List<FightStatModifier> modifiers =
+            new();
+
+        public FightUnitStats(
+            IReadOnlyDictionary<FightStatType, int> initialBaseValues)
+        {
+            if (initialBaseValues == null)
+            {
+                throw new ArgumentNullException(
+                    nameof(initialBaseValues));
+            }
+
+            foreach (
+                KeyValuePair<FightStatType, int> entry
+                in initialBaseValues)
+            {
+                baseValues[entry.Key] = entry.Value;
+            }
+        }
+
+        public int GetBaseValue(
+            FightStatType statType)
+        {
+            return baseValues.TryGetValue(
+                statType,
+                out int value)
+                ? value
+                : 0;
+        }
+
+        public int GetFinalValue(
+            FightStatType statType)
+        {
+            return FightStatCalculator.Calculate(
+                statType,
+                GetBaseValue(statType),
+                modifiers);
+        }
+
+        public void AddModifier(
+            FightStatModifier modifier)
+        {
+            modifiers.Add(modifier);
+        }
+
+        public bool RemoveModifier(
+            FightStatModifier modifier)
+        {
+            return modifiers.Remove(modifier);
+        }
+
+        public void ClearModifiers()
+        {
+            modifiers.Clear();
+        }
+    }
+}
