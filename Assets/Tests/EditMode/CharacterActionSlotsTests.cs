099cb88cd338f507418942e4da820ab5cd3803c0
JedrzejMakuch <jedrzejmakuch@gmail.com>
2026-07-21 20:31:35 +0200

    Add canonical character action slot order

---
+++ Assets/Tests/EditMode/CharacterActionSlotsTests.cs
@@ -0,0 +1,35 @@
+﻿using DiceBossArena.Game;
+using NUnit.Framework;
+
+public sealed class CharacterActionSlotsTests
+{
+    [Test]
+    public void All_ContainsSixSlotsInFixedOrder()
+    {
+        Assert.That(
+            CharacterActionSlots.Count,
+            Is.EqualTo(6));
+
+        Assert.That(
+            CharacterActionSlots.All,
+            Is.EqualTo(
+                new[]
+                {
+                    CharacterActionSlot.WeaponAttack,
+                    CharacterActionSlot.BasicAttack,
+                    CharacterActionSlot.SkillOne,
+                    CharacterActionSlot.SkillTwo,
+                    CharacterActionSlot.SkillThree,
+                    CharacterActionSlot.SkillFour
+                }));
+    }
+
+    [Test]
+    public void All_ReturnsSameReadOnlyCollection()
+    {
+        Assert.That(
+            CharacterActionSlots.All,
+            Is.SameAs(
+                CharacterActionSlots.All));
+    }
+}
---
+++ Assets/_Project/Scripts/Characters/Builds/CharacterActionSlots.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace DiceBossArena.Game
+{
+    public static class CharacterActionSlots
+    {
+        private static readonly
+            ReadOnlyCollection<CharacterActionSlot>
+                all =
+                    new List<CharacterActionSlot>
+                    {
+                        CharacterActionSlot.WeaponAttack,
+                        CharacterActionSlot.BasicAttack,
+                        CharacterActionSlot.SkillOne,
+                        CharacterActionSlot.SkillTwo,
+                        CharacterActionSlot.SkillThree,
+                        CharacterActionSlot.SkillFour
+                    }.AsReadOnly();
+
+        public static IReadOnlyList<CharacterActionSlot> All =>
+            all;
+
+        public static int Count =>
+            all.Count;
+    }
+}
