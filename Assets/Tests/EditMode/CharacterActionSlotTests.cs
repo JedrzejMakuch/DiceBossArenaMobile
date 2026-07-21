457b086ac4582c6ae14d9bb95c3cdc13527db9ef
JedrzejMakuch <jedrzejmakuch@gmail.com>
2026-07-21 20:25:41 +0200

    Add fixed character action slots

---
+++ Assets/Tests/EditMode/CharacterActionSlotTests.cs
@@ -0,0 +1,33 @@
+﻿using DiceBossArena.Game;
+using NUnit.Framework;
+
+public sealed class CharacterActionSlotTests
+{
+    [Test]
+    public void Slots_HaveExpectedFixedOrder()
+    {
+        Assert.That(
+            (int)CharacterActionSlot.WeaponAttack,
+            Is.EqualTo(0));
+
+        Assert.That(
+            (int)CharacterActionSlot.BasicAttack,
+            Is.EqualTo(1));
+
+        Assert.That(
+            (int)CharacterActionSlot.SkillOne,
+            Is.EqualTo(2));
+
+        Assert.That(
+            (int)CharacterActionSlot.SkillTwo,
+            Is.EqualTo(3));
+
+        Assert.That(
+            (int)CharacterActionSlot.SkillThree,
+            Is.EqualTo(4));
+
+        Assert.That(
+            (int)CharacterActionSlot.SkillFour,
+            Is.EqualTo(5));
+    }
+}
---
+++ Assets/_Project/Scripts/Characters/Builds/CharacterActionSlot.cs
@@ -0,0 +1,12 @@
+﻿namespace DiceBossArena.Game
+{
+    public enum CharacterActionSlot
+    {
+        WeaponAttack = 0,
+        BasicAttack = 1,
+        SkillOne = 2,
+        SkillTwo = 3,
+        SkillThree = 4,
+        SkillFour = 5
+    }
+}
