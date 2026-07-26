be3d2d5b274e5c059fcd2e91337062a5e410a212
JedrzejMakuch <jedrzejmakuch@gmail.com>
2026-07-26 20:27:17 +0200

    Add reusable text UI element factory

---
+++ Assets/Tests/EditMode/UI/Foundation/ReusableTextUIElementFactoryTests.cs
@@ -0,0 +1,128 @@
+﻿using DiceBossArena.UI;
+using NUnit.Framework;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace DiceBossArena.Tests.EditMode
+{
+    public sealed class ReusableTextUIElementFactoryTests
+    {
+        private GameObject factoryObject;
+        private GameObject containerObject;
+        private GameObject prefabObject;
+
+        private ReusableTextUIElementFactory factory;
+        private ReusableTextUIElement prefab;
+
+        [SetUp]
+        public void SetUp()
+        {
+            factoryObject =
+                new GameObject(
+                    "Reusable Text UI Element Factory");
+
+            containerObject =
+                new GameObject(
+                    "Container",
+                    typeof(RectTransform));
+
+            prefabObject =
+                new GameObject(
+                    "Reusable Text UI Element",
+                    typeof(RectTransform),
+                    typeof(CanvasRenderer),
+                    typeof(TextMeshProUGUI),
+                    typeof(ReusableTextUIElement));
+
+            factory =
+                factoryObject.AddComponent<
+                    ReusableTextUIElementFactory>();
+
+            prefab =
+                prefabObject.GetComponent<
+                    ReusableTextUIElement>();
+
+            AssignReference(
+                "prefab",
+                prefab);
+
+            AssignReference(
+                "container",
+                containerObject.transform);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(factoryObject);
+            Object.DestroyImmediate(containerObject);
+            Object.DestroyImmediate(prefabObject);
+        }
+
+        [Test]
+        public void Create_WithConfiguredContainer_CreatesElementUnderContainer()
+        {
+            ReusableTextUIElement created =
+                factory.Create();
+
+            Assert.That(created, Is.Not.Null);
+            Assert.That(created, Is.Not.SameAs(prefab));
+
+            Assert.That(
+                created.transform.parent,
+                Is.SameAs(containerObject.transform));
+        }
+
+        [Test]
+        public void Create_MissingContainer_UsesFactoryTransform()
+        {
+            AssignReference(
+                "container",
+                null);
+
+            ReusableTextUIElement created =
+                factory.Create();
+
+            Assert.That(created, Is.Not.Null);
+
+            Assert.That(
+                created.transform.parent,
+                Is.SameAs(factoryObject.transform));
+        }
+
+        [Test]
+        public void Create_MissingPrefab_LogsErrorAndReturnsNull()
+        {
+            AssignReference(
+                "prefab",
+                null);
+
+            LogAssert.Expect(
+                LogType.Error,
+                "ReusableTextUIElementFactory requires " +
+                "a ReusableTextUIElement prefab.");
+
+            ReusableTextUIElement created =
+                factory.Create();
+
+            Assert.That(created, Is.Null);
+        }
+
+        private void AssignReference(
+            string propertyName,
+            Object value)
+        {
+            SerializedObject serializedFactory =
+                new(factory);
+
+            SerializedProperty property =
+                serializedFactory.FindProperty(
+                    propertyName);
+
+            property.objectReferenceValue = value;
+            serializedFactory.ApplyModifiedPropertiesWithoutUndo();
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/UI/Foundation/ReusableTextUIElementFactory.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace DiceBossArena.UI
+{
+    public sealed class ReusableTextUIElementFactory :
+        MonoBehaviour
+    {
+        [SerializeField]
+        private ReusableTextUIElement prefab;
+
+        [SerializeField]
+        private Transform container;
+
+        public ReusableTextUIElement Create()
+        {
+            if (prefab == null)
+            {
+                Debug.LogError(
+                    $"{nameof(ReusableTextUIElementFactory)} requires " +
+                    $"a {nameof(ReusableTextUIElement)} prefab.",
+                    this);
+
+                return null;
+            }
+
+            Transform parent =
+                container != null
+                    ? container
+                    : transform;
+
+            return Instantiate(
+                prefab,
+                parent);
+        }
+    }
+}
