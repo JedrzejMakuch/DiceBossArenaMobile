9cdcd3f141cf57d9c813a4d7dea6d3690b9b0971
JedrzejMakuch <jedrzejmakuch@gmail.com>
2026-07-26 18:00:19 +0200

    Add UI screen history

---
+++ Assets/Tests/EditMode/UI/Foundation/UIScreenHistoryTests.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using DiceBossArena.UI;
+using NUnit.Framework;
+
+namespace DiceBossArena.Tests.EditMode
+{
+    public sealed class UIScreenHistoryTests
+    {
+        [Test]
+        public void Open_FirstScreen_SetsCurrentScreen()
+        {
+            UIScreenHistory<TestScreenId> history = new();
+
+            history.Open(TestScreenId.MainMenu);
+
+            Assert.That(history.HasCurrentScreen, Is.True);
+            Assert.That(
+                history.CurrentScreen,
+                Is.EqualTo(TestScreenId.MainMenu));
+            Assert.That(history.CanGoBack, Is.False);
+        }
+
+        [Test]
+        public void Open_SecondScreen_AddsPreviousScreenToHistory()
+        {
+            UIScreenHistory<TestScreenId> history = new();
+
+            history.Open(TestScreenId.MainMenu);
+            history.Open(TestScreenId.Inventory);
+
+            Assert.That(
+                history.CurrentScreen,
+                Is.EqualTo(TestScreenId.Inventory));
+            Assert.That(history.CanGoBack, Is.True);
+
+            bool didGoBack =
+                history.TryGoBack(out TestScreenId screenId);
+
+            Assert.That(didGoBack, Is.True);
+            Assert.That(
+                screenId,
+                Is.EqualTo(TestScreenId.MainMenu));
+            Assert.That(
+                history.CurrentScreen,
+                Is.EqualTo(TestScreenId.MainMenu));
+            Assert.That(history.CanGoBack, Is.False);
+        }
+
+        [Test]
+        public void Open_CurrentScreen_DoesNotDuplicateHistory()
+        {
+            UIScreenHistory<TestScreenId> history = new();
+
+            history.Open(TestScreenId.MainMenu);
+            history.Open(TestScreenId.Inventory);
+            history.Open(TestScreenId.Inventory);
+
+            Assert.That(history.TryGoBack(out _), Is.True);
+            Assert.That(
+                history.CurrentScreen,
+                Is.EqualTo(TestScreenId.MainMenu));
+            Assert.That(history.CanGoBack, Is.False);
+        }
+
+        [Test]
+        public void Replace_ChangesCurrentScreenWithoutAddingHistory()
+        {
+            UIScreenHistory<TestScreenId> history = new();
+
+            history.Open(TestScreenId.MainMenu);
+            history.Open(TestScreenId.Inventory);
+            history.Replace(TestScreenId.Settings);
+
+            Assert.That(
+                history.CurrentScreen,
+                Is.EqualTo(TestScreenId.Settings));
+
+            Assert.That(
+                history.TryGoBack(out TestScreenId screenId),
+                Is.True);
+
+            Assert.That(
+                screenId,
+                Is.EqualTo(TestScreenId.MainMenu));
+
+            Assert.That(history.CanGoBack, Is.False);
+        }
+
+        [Test]
+        public void TryGoBack_WithoutPreviousScreen_ReturnsFalse()
+        {
+            UIScreenHistory<TestScreenId> history = new();
+
+            history.Open(TestScreenId.MainMenu);
+
+            bool didGoBack =
+                history.TryGoBack(out TestScreenId screenId);
+
+            Assert.That(didGoBack, Is.False);
+            Assert.That(screenId, Is.EqualTo(default(TestScreenId)));
+            Assert.That(
+                history.CurrentScreen,
+                Is.EqualTo(TestScreenId.MainMenu));
+        }
+
+        [Test]
+        public void Clear_RemovesCurrentScreenAndHistory()
+        {
+            UIScreenHistory<TestScreenId> history = new();
+
+            history.Open(TestScreenId.MainMenu);
+            history.Open(TestScreenId.Inventory);
+
+            history.Clear();
+
+            Assert.That(history.HasCurrentScreen, Is.False);
+            Assert.That(history.CanGoBack, Is.False);
+
+            Assert.Throws<InvalidOperationException>(
+                () =>
+                {
+                    TestScreenId unused =
+                        history.CurrentScreen;
+                });
+        }
+
+        private enum TestScreenId
+        {
+            MainMenu,
+            Inventory,
+            Settings
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/UI/Foundation/UIScreenHistory.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiceBossArena.UI
+{
+    public sealed class UIScreenHistory<TScreenId>
+        where TScreenId : Enum
+    {
+        private readonly Stack<TScreenId> previousScreens =
+            new();
+
+        private TScreenId currentScreen;
+
+        public bool HasCurrentScreen { get; private set; }
+
+        public TScreenId CurrentScreen
+        {
+            get
+            {
+                if (!HasCurrentScreen)
+                {
+                    throw new InvalidOperationException(
+                        "Screen history has no current screen.");
+                }
+
+                return currentScreen;
+            }
+        }
+
+        public bool CanGoBack =>
+            previousScreens.Count > 0;
+
+        public void Open(TScreenId screenId)
+        {
+            if (HasCurrentScreen)
+            {
+                if (EqualityComparer<TScreenId>.Default.Equals(
+                        currentScreen,
+                        screenId))
+                {
+                    return;
+                }
+
+                previousScreens.Push(currentScreen);
+            }
+
+            currentScreen = screenId;
+            HasCurrentScreen = true;
+        }
+
+        public void Replace(TScreenId screenId)
+        {
+            currentScreen = screenId;
+            HasCurrentScreen = true;
+        }
+
+        public bool TryGoBack(out TScreenId screenId)
+        {
+            if (!CanGoBack)
+            {
+                screenId = default;
+                return false;
+            }
+
+            currentScreen = previousScreens.Pop();
+            screenId = currentScreen;
+            return true;
+        }
+
+        public void Clear()
+        {
+            previousScreens.Clear();
+            currentScreen = default;
+            HasCurrentScreen = false;
+        }
+    }
+}
