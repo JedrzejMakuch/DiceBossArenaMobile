6c972ce35398f32abd80161aef9937e99ddb0021
JedrzejMakuch <jedrzejmakuch@gmail.com>
2026-07-26 18:26:15 +0200

    Add UI back navigation controller

---
+++ Assets/Tests/EditMode/UI/Foundation/UIBackNavigationControllerTests.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using DiceBossArena.UI;
+using NUnit.Framework;
+
+namespace DiceBossArena.Tests.EditMode
+{
+    public sealed class UIBackNavigationControllerTests
+    {
+        [Test]
+        public void HandleBack_WithModal_ClosesModalFirst()
+        {
+            UIModalStack modalStack = new();
+            UIScreenRouter<TestScreenId> screenRouter =
+                CreateRouterWithHistory();
+
+            TestModalView modal = new();
+            modalStack.Push(modal);
+
+            int exitRequestCount = 0;
+
+            UIBackNavigationController<TestScreenId> controller =
+                new(
+                    modalStack,
+                    screenRouter,
+                    () => exitRequestCount++);
+
+            controller.HandleBack();
+
+            Assert.That(modalStack.HasModal, Is.False);
+            Assert.That(modal.IsVisible, Is.False);
+
+            Assert.That(
+                screenRouter.CurrentScreen,
+                Is.EqualTo(TestScreenId.Inventory));
+
+            Assert.That(screenRouter.CanGoBack, Is.True);
+            Assert.That(exitRequestCount, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void HandleBack_WithoutModal_GoesBackToPreviousScreen()
+        {
+            UIModalStack modalStack = new();
+            UIScreenRouter<TestScreenId> screenRouter =
+                CreateRouterWithHistory();
+
+            int exitRequestCount = 0;
+
+            UIBackNavigationController<TestScreenId> controller =
+                new(
+                    modalStack,
+                    screenRouter,
+                    () => exitRequestCount++);
+
+            controller.HandleBack();
+
+            Assert.That(
+                screenRouter.CurrentScreen,
+                Is.EqualTo(TestScreenId.MainMenu));
+
+            Assert.That(screenRouter.CanGoBack, Is.False);
+            Assert.That(exitRequestCount, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void HandleBack_WithoutModalOrHistory_RequestsExitConfirmation()
+        {
+            UIModalStack modalStack = new();
+            UIScreenRouter<TestScreenId> screenRouter = new();
+
+            TestUIView mainMenu = new();
+
+            screenRouter.Register(
+                TestScreenId.MainMenu,
+                mainMenu);
+
+            screenRouter.Open(TestScreenId.MainMenu);
+
+            int exitRequestCount = 0;
+
+            UIBackNavigationController<TestScreenId> controller =
+                new(
+                    modalStack,
+                    screenRouter,
+                    () => exitRequestCount++);
+
+            controller.HandleBack();
+
+            Assert.That(exitRequestCount, Is.EqualTo(1));
+            Assert.That(mainMenu.IsVisible, Is.True);
+
+            Assert.That(
+                screenRouter.CurrentScreen,
+                Is.EqualTo(TestScreenId.MainMenu));
+        }
+
+        [Test]
+        public void Constructor_NullModalStack_ThrowsException()
+        {
+            UIScreenRouter<TestScreenId> screenRouter = new();
+
+            Assert.Throws<ArgumentNullException>(
+                () => new UIBackNavigationController<TestScreenId>(
+                    null,
+                    screenRouter,
+                    () => { }));
+        }
+
+        [Test]
+        public void Constructor_NullScreenRouter_ThrowsException()
+        {
+            UIModalStack modalStack = new();
+
+            Assert.Throws<ArgumentNullException>(
+                () => new UIBackNavigationController<TestScreenId>(
+                    modalStack,
+                    null,
+                    () => { }));
+        }
+
+        [Test]
+        public void Constructor_NullExitCallback_ThrowsException()
+        {
+            UIModalStack modalStack = new();
+            UIScreenRouter<TestScreenId> screenRouter = new();
+
+            Assert.Throws<ArgumentNullException>(
+                () => new UIBackNavigationController<TestScreenId>(
+                    modalStack,
+                    screenRouter,
+                    null));
+        }
+
+        private static UIScreenRouter<TestScreenId>
+            CreateRouterWithHistory()
+        {
+            UIScreenRouter<TestScreenId> screenRouter = new();
+
+            screenRouter.Register(
+                TestScreenId.MainMenu,
+                new TestUIView());
+
+            screenRouter.Register(
+                TestScreenId.Inventory,
+                new TestUIView());
+
+            screenRouter.Open(TestScreenId.MainMenu);
+            screenRouter.Open(TestScreenId.Inventory);
+
+            return screenRouter;
+        }
+
+        private enum TestScreenId
+        {
+            MainMenu,
+            Inventory
+        }
+
+        private sealed class TestUIView : IUIView
+        {
+            public bool IsVisible { get; private set; }
+
+            public void Show()
+            {
+                IsVisible = true;
+            }
+
+            public void Hide()
+            {
+                IsVisible = false;
+            }
+        }
+
+        private sealed class TestModalView : IUIModalView
+        {
+            public bool IsVisible { get; private set; }
+            public bool IsInputEnabled { get; private set; }
+
+            public void Show()
+            {
+                IsVisible = true;
+            }
+
+            public void Hide()
+            {
+                IsVisible = false;
+            }
+
+            public void SetInputEnabled(bool isEnabled)
+            {
+                IsInputEnabled = isEnabled;
+            }
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/UI/Foundation/UIBackNavigationController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DiceBossArena.UI
+{
+    public sealed class UIBackNavigationController<TScreenId>
+        where TScreenId : Enum
+    {
+        private readonly UIModalStack modalStack;
+        private readonly UIScreenRouter<TScreenId> screenRouter;
+        private readonly Action requestExitConfirmation;
+
+        public UIBackNavigationController(
+            UIModalStack modalStack,
+            UIScreenRouter<TScreenId> screenRouter,
+            Action requestExitConfirmation)
+        {
+            this.modalStack = modalStack ??
+                throw new ArgumentNullException(
+                    nameof(modalStack));
+
+            this.screenRouter = screenRouter ??
+                throw new ArgumentNullException(
+                    nameof(screenRouter));
+
+            this.requestExitConfirmation =
+                requestExitConfirmation ??
+                throw new ArgumentNullException(
+                    nameof(requestExitConfirmation));
+        }
+
+        public void HandleBack()
+        {
+            if (modalStack.TryPop())
+            {
+                return;
+            }
+
+            if (screenRouter.TryGoBack())
+            {
+                return;
+            }
+
+            requestExitConfirmation.Invoke();
+        }
+    }
+}
