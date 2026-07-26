36129210124cea49cfe6759ee081c606df2c7853
JedrzejMakuch <jedrzejmakuch@gmail.com>
2026-07-26 18:29:29 +0200

    Add UI back input source

---
+++ Assets/_Project/Scripts/UI/Foundation/UIBackInputSource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace DiceBossArena.UI
+{
+    public sealed class UIBackInputSource : MonoBehaviour
+    {
+        public event Action BackPressed;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                BackPressed?.Invoke();
+            }
+        }
+    }
+}
