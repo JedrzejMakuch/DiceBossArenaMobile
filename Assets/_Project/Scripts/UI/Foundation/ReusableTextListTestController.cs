57cf723e4e1caf517e615f6fed15b85f4c885544
JedrzejMakuch <jedrzejmakuch@gmail.com>
2026-07-26 21:19:42 +0200

    Add reusable text list PlayMode test scene

---
+++ Assets/_Project/Scripts/UI/Foundation/ReusableTextListTestController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace DiceBossArena.UI
+{
+    public sealed class ReusableTextListTestController :
+        MonoBehaviour
+    {
+        [SerializeField]
+        private ReusableTextList list;
+
+        [ContextMenu("Show Five Items")]
+        public void ShowFiveItems()
+        {
+            list.SetItems(
+                new[]
+                {
+                    "First",
+                    "Second",
+                    "Third",
+                    "Fourth",
+                    "Fifth"
+                });
+        }
+
+        [ContextMenu("Update Five Items")]
+        public void UpdateFiveItems()
+        {
+            list.SetItems(
+                new[]
+                {
+                    "Updated First",
+                    "Updated Second",
+                    "Updated Third",
+                    "Updated Fourth",
+                    "Updated Fifth"
+                });
+        }
+
+        [ContextMenu("Show Two Items")]
+        public void ShowTwoItems()
+        {
+            list.SetItems(
+                new[]
+                {
+                    "Only First",
+                    "Only Second"
+                });
+        }
+
+        [ContextMenu("Clear Items")]
+        public void ClearItems()
+        {
+            list.Clear();
+        }
+    }
+}
