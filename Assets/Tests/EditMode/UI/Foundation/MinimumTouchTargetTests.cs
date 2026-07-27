bd4aed9c67cfa1af824f68136c05d1c4beefafd0
JedrzejMakuch <jedrzejmakuch@gmail.com>
2026-07-27 19:27:21 +0200

    Add minimum touch target standard

---
+++ Assets/Tests/EditMode/UI/Foundation/MinimumTouchTargetTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DiceBossArena.UI.Tests
+{
+    public sealed class MinimumTouchTargetTests
+    {
+        private GameObject testObject;
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(testObject);
+        }
+
+        [Test]
+        public void AddingComponent_AppliesDefaultMinimumSize()
+        {
+            testObject =
+                new GameObject(
+                    "TouchTarget",
+                    typeof(RectTransform),
+                    typeof(LayoutElement));
+
+            testObject.AddComponent<MinimumTouchTarget>();
+
+            LayoutElement layoutElement =
+                testObject.GetComponent<LayoutElement>();
+
+            Assert.That(
+                layoutElement.minWidth,
+                Is.EqualTo(96f));
+
+            Assert.That(
+                layoutElement.minHeight,
+                Is.EqualTo(96f));
+        }
+
+        [Test]
+        public void AddingComponent_DoesNotChangePreferredSize()
+        {
+            testObject =
+                new GameObject(
+                    "TouchTarget",
+                    typeof(RectTransform),
+                    typeof(LayoutElement));
+
+            LayoutElement layoutElement =
+                testObject.GetComponent<LayoutElement>();
+
+            layoutElement.preferredWidth = 160f;
+            layoutElement.preferredHeight = 120f;
+
+            testObject.AddComponent<MinimumTouchTarget>();
+
+            Assert.That(
+                layoutElement.preferredWidth,
+                Is.EqualTo(160f));
+
+            Assert.That(
+                layoutElement.preferredHeight,
+                Is.EqualTo(120f));
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/UI/Foundation/MinimumTouchTarget.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DiceBossArena.UI
+{
+    [RequireComponent(typeof(RectTransform))]
+    [RequireComponent(typeof(LayoutElement))]
+    public sealed class MinimumTouchTarget :
+        MonoBehaviour
+    {
+        private const float DefaultMinimumSize = 96f;
+
+        [SerializeField]
+        [Min(1f)]
+        private float minimumWidth =
+            DefaultMinimumSize;
+
+        [SerializeField]
+        [Min(1f)]
+        private float minimumHeight =
+            DefaultMinimumSize;
+
+        private LayoutElement layoutElement;
+
+        private void Awake()
+        {
+            ApplyMinimumSize();
+        }
+
+        private void OnValidate()
+        {
+            ApplyMinimumSize();
+        }
+
+        private void ApplyMinimumSize()
+        {
+            if (layoutElement == null)
+            {
+                layoutElement =
+                    GetComponent<LayoutElement>();
+            }
+
+            layoutElement.minWidth =
+                minimumWidth;
+
+            layoutElement.minHeight =
+                minimumHeight;
+        }
+    }
+}
